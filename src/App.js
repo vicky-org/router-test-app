030b4329f22fafcf280bacfc13dc2d8109f719f9
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-10-03 23:37:51 +0530

    route parameters

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
-// import ProductDetails from "./components/productDetails";
+import ProductDetails from "./components/productDetails";
 // import NotFound from "./components/notFound";
 import "./App.css";
 
@@ -16,11 +16,12 @@ class App extends Component {
         <NavBar />
         <div className="content">
           <Switch>
+            <Route path="/products/:id" component={ProductDetails} />
             <Route
               path="/products"
-              render={(props) => <Products sortBy="newest" {...props} />}
+              render={(props) => <Products sortBy="nearest" {...props} />}
             />
-            <Route path="/posts" component={Posts} />
+            <Route path="/posts/:year/:month" component={Posts} />
             <Route path="/admin" component={Dashboard} />
             <Route path="/" component={Home} />
           </Switch>

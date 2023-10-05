8d1008295ebe38bf23aa9eb006a5f5a52e8af381
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-10-05 14:48:44 +0530

    redirects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
-// import NotFound from "./components/notFound";
+import NotFound from "./components/notFound";
 import "./App.css";
 
 class App extends Component {
@@ -23,7 +23,10 @@ class App extends Component {
             />
             <Route path="/posts/:year?/:month?" component={Posts} />
             <Route path="/admin" component={Dashboard} />
-            <Route path="/" component={Home} />
+            <Redirect from="/messages" to="/posts" />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </div>
